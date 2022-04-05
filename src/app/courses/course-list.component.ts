b256014df7ba91dd5faf3e8df11b400005adce74
EricEOL <lopes.eric051@gmail.com>
2022-04-05 19:31:58 -0300

    Criação do Star component para exibição do rating

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -14,7 +14,7 @@ export class CourseListComponent implements OnInit {
         {
           id: 1,
           name: "Angular Forms",
-          imageUrl: "",
+          imageUrl: "/assets/images/forms.png",
           price: 99.99,
           code: "SSSD-3383",
           duration: 45,
@@ -24,7 +24,7 @@ export class CourseListComponent implements OnInit {
         {
           id: 2,
           name: "Angular HTTP",
-          imageUrl: "",
+          imageUrl: "/assets/images/http.png",
           price: 104.99,
           code: "SXSD-12332",
           duration: 45,
