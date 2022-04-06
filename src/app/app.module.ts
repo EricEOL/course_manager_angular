452d27f443c47ff289266b1610c0f1f4dac997eb
EricEOL <lopes.eric051@gmail.com>
2022-04-06 19:06:23 -0300

    movendo component de erro para pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    CoreModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', 
         redirectTo: 'courses',
         pathMatch: 'full'
-      },
-      {
-        path: '**',
-        component: Error404Component
       }
     ])
   ],
