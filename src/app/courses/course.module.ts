4eaf6305aeb096363edf1107aaacd1e3cb59a20f
EricEOL <lopes.eric051@gmail.com>
2022-04-06 18:55:37 -0300

    movendo navbar para pasta core

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -4,22 +4,22 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
+import { PipeModule } from "../shared/pipe/app-pipe.module";
 
 @NgModule({
   declarations: [
     CourseListComponent,
     CourseInfoComponent,
-    StarComponent,
-    ReplacePipe,
   ],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
+    StarModule,
+    PipeModule,
     RouterModule.forChild([
       {
         path: 'courses',
